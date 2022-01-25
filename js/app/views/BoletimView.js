75ea5f5bda28fc5a1b0a2b3193def4bd179704c9
Elias Lima <38722114+EliasRLima@users.noreply.github.com>
2022-01-25 14:41:34 -0300

    media-aluno
    Adicionado a media de cada aluno para a tabela.

--- js/app/views/BoletimView.js
+++ js/app/views/BoletimView.js
@@ -18,7 +18,9 @@ class BoletimView extends View {
                     <th>FREQUENCIA</th>
                     <th>Nota 1</th>
                     <th>Nota 2</th>
+                    <th>Media</th>
                     <th>Nota final</th>
+                    <th>Media final</th>
                     <th>Situacao</th>
                 </tr>
             </thead>
@@ -31,7 +33,9 @@ class BoletimView extends View {
                     <td>${aluno.getFrequencia().getFrequencia()}</td>
                     <td>${aluno.getNotas().getNota1()}</td>
                     <td>${aluno.getNotas().getNota2()}</td>
+                    <td>${aluno.getNotas().getMedia()}</td>
                     <td>${aluno.getNotas().getNota3()}</td>
+                    <td>${aluno.getNotas().getMediaFinal()}</td>
                     <td>${aluno.getSituacao()}</td>
                     </tr>
                     
@@ -39,9 +43,9 @@ class BoletimView extends View {
             </tbody>
 
             <tfoot>
-                <td>Aprovados</td>
+                <td colspan="2">Aprovados</td>
                 <td>${boletim.getAlunos().filter(aprovado).length}</td>
-                <td>Reprovados</td>
+                <td colspan="2">Reprovados</td>
                 <td>${boletim.getAlunos().filter(reprovado).length}</td>
                 <td>Media da Turma</td>
                 <td>${(Number(boletim.getAlunos().reduce(soma,0.0)) / boletim.getAlunos().length).toFixed(2)}</td>
